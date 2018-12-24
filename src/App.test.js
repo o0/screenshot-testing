595d15d03ad711d688e869c1c6823ce3e1d3c356
o0 <igor.alexeenko@htmlacademy.ru>
2018-12-24 04:43:51 +0300

    Full page rendering instead of rendering only component

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
+import { readFile } from 'fs';
+import { promisify } from 'util';
 import { toMatchImageSnapshot  } from 'jest-image-snapshot';
 import puppeteer from 'puppeteer';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from './App';
 
+const asyncReadFile = promisify(readFile);
+
+
 expect.extend({toMatchImageSnapshot });
+
 
 const getScreenshot = async (markdown) => {
   const browser = await puppeteer.launch();
@@ -17,15 +23,26 @@ const getScreenshot = async (markdown) => {
 }
 
 
+const getFullIndexPageMarkdown = async (component) => {
+  const markdown = await asyncReadFile(`${__dirname}/../public/index.html`, 'utf-8');
+  const componentMarkdown = ReactDOMServer.renderToStaticMarkup(component);
+  const newMarkdown = markdown
+    .replace(/%PUBLIC_URL%/g, '/')
+    .replace('<div id="root"></div>', `<div id="root">${componentMarkdown}</div>`);
+
+  return newMarkdown;
+}
+
+
 it('<App /> is rendered to snapshot correctly', async () => {
-  expect(
-    await getScreenshot(ReactDOMServer.renderToStaticMarkup(<App />))
-  ).toMatchImageSnapshot();
+  expect(await getScreenshot(
+    await getFullIndexPageMarkdown(<App />)
+  )).toMatchImageSnapshot();
 });
 
 
 it('Failing test', async () => {
-  expect(
-    await getScreenshot(ReactDOMServer.renderToStaticMarkup(<App foo="String that breaks testing" />))
-  ).toMatchImageSnapshot();
+  expect(await getScreenshot(
+    await getFullIndexPageMarkdown(<App foo="Incorrect value of prop" />)
+  )).toMatchImageSnapshot();
 });
